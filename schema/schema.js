62891738a77fac772ed0ac8b192c925dd101a7db
carrymycross7 <threet.ldustin@gmail.com>
2024-02-16 09:41:36 -0600

    SUPPORT7 add delete mutation

--- schema/schema.js
+++ schema/schema.js
@@ -1,6 +1,5 @@
 const graphql = require('graphql');
 const axios = require('axios');
-const {response} = require("express");
 
 const {
     GraphQLObjectType,
@@ -46,6 +45,16 @@ const mutation = new GraphQLObjectType({
                 return axios.post('http://localhost:8000/worlds', {name,description})
                     .then(res => res.data)
             }
+        },
+        deleteLocation: {
+          type: LocationType,
+          args: {
+              id: {type: new GraphQLNonNull(GraphQLString)}
+          },
+          resolve(parentValue, {id}) {
+              return axios.delete(`http://localhost:8000/worlds/${id}`)
+                  .then(res => res.data)
+          }
         },
         editLocation: {
             type: LocationType,
