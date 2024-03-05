1863979a8dd79706a8bd5f5f0c4800a0b23f8dd5
carrymycross7 <threet.ldustin@gmail.com>
2024-03-04 21:14:58 -0600

    wip

--- client/components/App.js
+++ client/components/App.js
@@ -1,5 +1,34 @@
 import React from "react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery
+} from '@apollo/client';
+import gql from "graphql-tag";
 
-export default () => {
-  return <div>APP!!</div>
+
+// Initialize Apollo Client
+const client = new ApolloClient({
+  uri: 'http://localhost:27017/minecraft',
+  cache: new InMemoryCache()
+});
+
+const GET_LOCATIONS = gql`
+  query AllLocations {
+    minecraft {
+      description
+    }
+  }
+`;
+
+function App() {
+  const { loading, error, data } = useQuery(GET_LOCATIONS);
+  return (
+      <ApolloProvider client={client}>
+        <div>APP</div>
+      </ApolloProvider>
+  );
 }
+
+export default App;
